e4208d8503926debe0b4d42efa603c7e770a55c2
terrencejihoonjung <terrencejung928@gmail.com>
2025-01-31 21:52:35 -0800

    Fix linting errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,6 +62,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
+    console.error("Database Error:", error);
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -100,6 +101,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error("Database Error:", error);
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
